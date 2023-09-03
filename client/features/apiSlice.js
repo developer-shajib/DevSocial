e62cb622e6c918589ebc1e9abd35fa335551d3df
shajib <sunhailshajib1@gmail.com>
2023-09-03 19:21:17 +0600

    Change .env file base url

--- client/features/apiSlice.js
+++ client/features/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 // Create Api Slice
 const apiSlice = createApi({
   tagTypes: ['Me'],
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5050', credentials: 'include' }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_SERVER_URL, credentials: 'include' }),
   endpoints: (build) => ({})
 });
 
