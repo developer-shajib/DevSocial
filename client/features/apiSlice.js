16b7c29e0db5e70c6dfb61d56c227c16597fe7a6
shajib <sunhailshajib1@gmail.com>
2023-09-03 22:25:30 +0600

    Fiexed error and bug

--- client/features/apiSlice.js
+++ client/features/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 // Create Api Slice
 const apiSlice = createApi({
   tagTypes: ['Me'],
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://dev-social-s1li.onrender.com', credentials: 'include' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://dev-social-s1li.onrender.com', credentials: 'include', withCredentials: true }),
   endpoints: (build) => ({})
 });
 
